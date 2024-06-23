ca8078d90cfab8edb774667394e8ac8583cb771f
agomolka <axrozwadowska@gmail.com>
2024-06-23 20:32:33 +0200

    Initial commit

---
+++ src/test/java/com/example/testing_poc/TestingPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
